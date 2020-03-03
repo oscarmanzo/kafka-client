1298581fa8ee0a45701e3d1ca30209542e4e7f7e
Oscar Manzo <oscar.manzo.c@gmail.com>
2020-03-03 00:12:47 -0600

    Consumer demo

---
+++ src/main/java/com/kafka/client/producer/ConsumerDemo.java
@@ -0,0 +1,54 @@
+package com.kafka.client.producer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final String bootstrapServers = "127.0.0.1:9092";
+    private static final String groupId = "my-group-id";
+    private static final String resetOffset = "earliest";
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+    private static final Properties properties = new Properties();
+
+    static {
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, resetOffset);
+    }
+
+    public static void main(String[] args) {
+
+        final String topic = "my-topic";
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord record : records) {
+                print(record);
+            }
+        }
+
+    }
+
+    private static void print(ConsumerRecord record) {
+        logger.info("key:"+ record.key() +" ");
+        logger.info("value:"+ record.value() +" ");
+        logger.info("partition:"+ record.partition() +" ");
+        logger.info("offset:"+ record.offset() +"\n");
+    }
+
+}
