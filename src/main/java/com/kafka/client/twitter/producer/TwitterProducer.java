a7a1d58e8add9e2611f52da13496cbbb5260af7b
Oscar Manzo <oscar.manzo.c@gmail.com>
2020-03-24 23:59:49 -0600

    addind a kafka producer to publish the tweets

--- src/main/java/com/kafka/client/twitter/producer/TwitterProducer.java
+++ src/main/java/com/kafka/client/twitter/producer/TwitterProducer.java
@@ -10,10 +10,15 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -37,11 +42,20 @@ public class TwitterProducer {
         Client client = createTwitterClient(msgQueue);
         client.connect();
 
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+        // It runs when app exits
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            client.stop();
+            producer.close();
+        }));
+
         while (!client.isDone()) {
             String msg = null;
 
             try {
                 msg = msgQueue.poll(5, TimeUnit.SECONDS);
+                publish(producer, msg);
             } catch (InterruptedException e) {
                 e.printStackTrace();
                 client.stop();
@@ -84,6 +98,37 @@ public class TwitterProducer {
         Client hosebirdClient = builder.build();
         return hosebirdClient;
         //hosebirdClient.connect();
+    }
+
+    private void publish(final KafkaProducer<String, String> producer, final String msg) {
+        final String topic = "twitter_tweets";
+        final ProducerRecord<String, String> record = new ProducerRecord<>(topic, null, msg);
+
+        //producer.send(record);
+        producer.send(record, (metadata, exception) -> {
+            if (exception == null) {
+                logger.info("metadata\n" +
+                        "topic:"+ metadata.topic() +"\n" +
+                        "partition:"+ metadata.partition() +"\n" +
+                        "offset:"+ metadata.offset() +"\n" +
+                        "timestamp:"+ metadata.timestamp());
+            } else {
+                logger.error("error", exception);
+            }
+        });
+    }
+
+    private KafkaProducer<String, String> createKafkaProducer() {
+        final String bootstrapServers = "127.0.0.1:9092";
+
+        final Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        return producer;
     }
 
 }
